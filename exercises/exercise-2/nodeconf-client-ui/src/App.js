f3295842f49407c1747514df6c93ad22e5f91962
toblich <toblich@gmail.com>
2018-10-15 16:57:07 -0300

    Add comments on lines for SDK integration

--- exercises/exercise-2/nodeconf-client-ui/src/App.js
+++ exercises/exercise-2/nodeconf-client-ui/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component, PureComponent } from 'react';
 import './App.css';
-import {DefaultApi} from 'nodeconf-api';
 
-// ---
+/* Import the client SDK */
+import {DefaultApi} from 'nodeconf-api';
 
 const TWITTER_ICON_URL = 'https://pbs.twimg.com/profile_images/1013798240683266048/zRim1x6M_400x400.jpg';
 const GITHUB_ICON = 'https://slack-files2.s3-us-west-2.amazonaws.com/avatars/2017-12-19/288981919427_f45f04edd92902a96859_512.png';
 
-// ---
 
+/* Instantiate the SDK */
 const api = new DefaultApi();
+
 
 class App extends Component {
   constructor(props) {
@@ -20,6 +21,7 @@ class App extends Component {
   }
 
   componentDidMount() {
+    /* Use the client SDK to call the Backend */
     api.editionsEditionIdSpeakersGet(18)
       .then(speakers => this.setState({speakers}))
     ;
@@ -53,9 +55,11 @@ class SpeakersList extends PureComponent {
       <div className='speaker' key={'speaker-' + number}>
         <div>
           <span className='speaker-name'>{speaker.name}</span>
+
           <a href={'https://twitter.com/' + speaker.twitterAccount}>
             <img className='social-icon' src={TWITTER_ICON_URL} alt='twitter'/>
           </a>
+
           <a href={'https://github.com/' + speaker.githubAccount.replace('@', '')}>
             <img className='social-icon' src={GITHUB_ICON} alt='github'/>
           </a>
